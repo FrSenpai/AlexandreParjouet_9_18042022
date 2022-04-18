40a8660731b983cfa9d5c6b04365864756ea27e8
Alex <collo@DESKTOP-0MTRO7E>
2022-04-18 20:21:56 +0200

    fix can't edit bill

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -87,7 +87,7 @@ export default class {
 
   handleEditTicket(e, bill, bills) {
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
-    if (this.id === undefined || this.id !== bill.id) this.id = bill.id
+    // if (this.id === undefined || this.id !== bill.id) this.id = bill.id
     if (this.counter % 2 === 0) {
       bills.forEach(b => {
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
