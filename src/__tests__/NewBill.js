db573cccab467b8673726f6d834b4d85850c2c04
Alex <collo@DESKTOP-0MTRO7E>
2022-05-24 21:16:48 +0200

    handle update bills query error 500 & 404

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -13,10 +13,12 @@ import {
   localStorageMock
 } from "../__mocks__/localStorage.js";
 import store from "../__mocks__/store.js";
+import mockStore from "../__mocks__/store"
 import {
-  ROUTES
+  ROUTES,
 } from "../constants/routes.js";
-
+import Router from "../app/Router.js";
+jest.mock("../app/store", () => mockStore)
 Object.defineProperty(window, "localStorage", {
   value: localStorageMock,
 });
@@ -46,7 +48,7 @@ const bill = {
   email: "a@a",
   pct: 20
 }
-beforeEach(() => {
+function generateNewBillPage() {
   const html = NewBillUI()
   document.body.innerHTML = html
   const PREVIOUS_LOCATION = ""
@@ -58,8 +60,7 @@ beforeEach(() => {
     store: store
   });
 
-})
-
+}
 function getFakeFile(format) {
   return {
     target: {
@@ -68,6 +69,21 @@ function getFakeFile(format) {
       })],
     }
   }
+}
+
+function doFailedQuery(error) {
+  mockStore.bills.mockImplementationOnce(() => {
+    return {
+      update: () => {
+        return Promise.reject(new Error(error))
+      }
+    }
+  })
+  const htmlWithError = BillsUI({error})
+  document.body.innerHTML = htmlWithError
+  // await new Promise(process.nextTick);
+  let message = screen.getByText(error)
+  expect(message).toBeTruthy()
 }
 /**
  * @description update DOM to set many values needed by the new bill form
@@ -97,7 +113,11 @@ function updateDomToCreateBills(needToBeValid) {
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
     describe("When I fill the form", () => {
+
       describe("When I add a new VALID file", () => {
+        beforeEach(() => {
+          generateNewBillPage()
+        })
         test("Then it (file input) should be updated", () => {
           const submit = screen.getAllByTestId("file")
           const handleChange = jest.fn((e) => {
@@ -115,6 +135,9 @@ describe("Given I am connected as an employee", () => {
       })
 
       describe("When I add a new INVALID file", () => {
+        beforeEach(() => {
+          generateNewBillPage()
+        })
         test("Then it (file input) should not be updated and files array should not contain it", () => {
           const submit = screen.getAllByTestId("file")
           const handleChange = jest.fn((e) => {
@@ -132,7 +155,11 @@ describe("Given I am connected as an employee", () => {
       })
 
       describe("When I submit the form with valid data", () => {
-        test("Then we should be redirected to the bill list", () => {
+        beforeEach(() => {
+          generateNewBillPage()
+
+        })
+        test("Then we should be redirected to the bill list and the POST query should be successfull", () => {
           //checking if we've got the valid html
           expect(screen.getAllByText("Type de dépense")).toBeTruthy()
           //handling submit 
@@ -146,14 +173,48 @@ describe("Given I am connected as an employee", () => {
 
           //formating & updating data
           updateDomToCreateBills(true)
-         
+
           fireEvent.submit(submit[0])
           expect(screen.getAllByText("Mes notes de frais")).toBeTruthy()
         })
+        
       })
 
-      
+      describe("When I submit the form with valid data but the API query fail", () => {
+        beforeEach(() => {
+          jest.spyOn(mockStore, "bills")
+          Object.defineProperty(
+            window,
+            'localStorage', {
+              value: localStorageMock
+            }
+          )
+          window.localStorage.setItem('user', JSON.stringify({
+            type: 'Employee',
+            email: "a@a"
+          }))
+          const root = document.createElement("div")
+          root.setAttribute("id", "root")
+
+          document.body.appendChild(root)
+          Router()
+          
+        })
+        test("update bills from an API and fails with 404 message error", async () => {
+
+          doFailedQuery("Erreur 404")
+        })
+
+        test("update bills from an API and fails with 500 message error", async () => {
+
+          doFailedQuery("Erreur 500")
+        })
+      })
+
       describe("When I submit the form with invalid data", () => {
+        beforeEach(() => {
+          generateNewBillPage()
+        })
         test("Then we should stay at the same page and not be redirected", () => {
           //checking if we've got the valid html
           expect(screen.getAllByText("Type de dépense")).toBeTruthy()
@@ -168,7 +229,7 @@ describe("Given I am connected as an employee", () => {
 
           //formating & updating data
           updateDomToCreateBills(false)
-          
+
           fireEvent.submit(submit[0])
           expect(screen.getByText("Type de dépense")).toBeTruthy()
         })
